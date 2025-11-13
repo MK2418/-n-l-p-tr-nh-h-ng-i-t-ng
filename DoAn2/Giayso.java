c1e3c78b0d0501eb74450eb2a12aa85aa810b9ce
MK2418 <tranminhkiet090@gmail.com>
2025-11-13 19:01:05 +0700

    Update Giayso.java

--- DoAn2/Giayso.java
+++ DoAn2/Giayso.java
@@ -3,6 +3,7 @@ public class Giayso extends Dodunghoctap {
     public String doDay;
     public String khoGiay;
 
+    // Hàm có tham số
     public Giayso(String maSP, String tenSP, int soLuong, int donGia,
             String donViTinh, String maLoai, String maSX,
             String doDay, String khoGiay) {
@@ -11,6 +12,33 @@ public class Giayso extends Dodunghoctap {
         this.khoGiay = khoGiay;
     }
 
+    // Hàm không tham số
+    public Giayso() {
+        super();
+        doDay = "";
+        khoGiay = "";
+    }
+
+    // Hàm thiết lập sao chép
+    public Giayso(Giayso other) {
+        super(other.maSP, other.tenSP, other.soLuong, other.donGia,
+                other.donViTinh, other.maLoai, other.maSX);
+        this.doDay = other.doDay;
+        this.khoGiay = other.khoGiay;
+    }
+
+    // Phương thức nhập
+    @Override
+    public void nhap() {
+        super.nhap(); // Gọi phương thức nhập từ lớp cha
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.print("Nhap do day: ");
+        doDay = sc.nextLine();
+        System.out.print("Nhap kho giay: ");
+        khoGiay = sc.nextLine();
+    }
+
+    // Phương thức xuất
     @Override
     public void xuat() {
         String ten = tenSP.length() > 15 ? tenSP.substring(0, 12) + "..." : tenSP;
@@ -24,19 +52,37 @@ public class Giayso extends Dodunghoctap {
                 maSP, tenSP, soLuong, donGia, donViTinh, maLoai, maSX, doDay, khoGiay);
     }
 
-    public String getdoDay() {
+    // Getter methods
+    public String getDoDay() {
         return doDay;
     }
 
-    public String getkhoGiay() {
+    public String getKhoGiay() {
         return khoGiay;
     }
 
-    public void setdoDay(String doDay) {
+    // Setter methods
+    public void setDoDay(String doDay) {
         this.doDay = doDay;
     }
 
-    public void setkhoGiay(String khoGiay) {
+    public void setKhoGiay(String khoGiay) {
         this.khoGiay = khoGiay;
+    }
+
+    // Phương thức toString
+    @Override
+    public String toString() {
+        return "Giayso{" +
+                "maSP='" + maSP + '\'' +
+                ", tenSP='" + tenSP + '\'' +
+                ", soLuong=" + soLuong +
+                ", donGia=" + donGia +
+                ", donViTinh='" + donViTinh + '\'' +
+                ", maLoai='" + maLoai + '\'' +
+                ", maSX='" + maSX + '\'' +
+                ", doDay='" + doDay + '\'' +
+                ", khoGiay='" + khoGiay + '\'' +
+                '}';
     }
 }
